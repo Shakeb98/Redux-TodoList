aeced77a47214d9b53f3471a8861a3bdcd4debd8
Shakeeb Khan <shakeebkhan@TTNs-MacBook-Pro.local>
2022-11-09 11:42:25 +0530

    Todolist

--- src/screens/WorkProject.js
+++ src/screens/WorkProject.js
@@ -45,13 +45,13 @@ export class WorkProject extends Component {
                                     <TouchableOpacity
                                         style={styles.Add}
                                         onPress={() => this.navigationForEdit(item)}>
-                                        <Text>Edit</Text>
+                                        <Text style={styles.btnText}>Edit</Text>
                                     </TouchableOpacity>
                                     <TouchableOpacity style={styles.Add}
                                         onPress={() => {
                                             this.deleteItemById(item.Id)
                                         }}
-                                    ><Text>Delete</Text></TouchableOpacity>
+                                    ><Text style={styles.btnText}>Delete</Text></TouchableOpacity>
                                 </View>
                             </View>
                         )
@@ -83,10 +83,16 @@ const styles = StyleSheet.create({
     HeadingText: {
         fontSize: 20,
         fontWeight: 'bold',
+        top:10
+    },
+    titleText:{
+        top:10,
+        fontWeight:'bold',
     },
     AddEdit: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+        top:13
     },
     Add: {
         backgroundColor: 'gray',
@@ -95,6 +101,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius:20,
         justifyContent:'center',
+    },
+    MyList:{
+        borderBottomWidth:2,
+        borderBottomColor:'black',
+        top:10
+    },
+    btnText:{
+        fontWeight:'bold',
+        color:'white'
     }
 })
 
